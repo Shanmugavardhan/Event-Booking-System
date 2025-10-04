656b70b8e1e8c9d1588f2af783ddb890ed664156
Shanmugavardhan <shanmugavardhan5@gmail.com>
2025-10-04 22:57:36 +0530

    pages files created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,17 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
 import './App.css';
 
 function App() {
   return (
-    <div>
-      <h1>Event Booking System</h1>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<h1>Welcome to the Event Booking System</h1>} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </Router>
   );
 }
 
