f422a19bbcabd9ece524a3a6d18b8a347b9a2494
Shanmugavardhan <shanmugavardhan5@gmail.com>
2025-10-04 20:52:12 +0530

    SignIn and SignUp Backend complete

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from flask_cors import CORS
 from database import engine, Base
+from routes.auth import auth_bp 
 
 app = Flask(__name__)
 CORS(app)
 
 Base.metadata.create_all(bind=engine)
+app.register_blueprint(auth_bp, url_prefix='/auth')
 
 @app.route("/")
 def home():
