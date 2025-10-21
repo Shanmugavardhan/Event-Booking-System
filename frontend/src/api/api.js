c5eef67f7978432cd6bbbedaf186147018ac520a
Shanmugavardhan <shanmugavardhan5@gmail.com>
2025-10-21 11:27:07 +0530

    Final submisson for hosting

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://127.0.0.1:5000/auth",
+  baseURL: "https://flask-backend-656829456717.asia-south1.run.app/auth",
   headers: {
     "Content-type": "application/json",
   },
